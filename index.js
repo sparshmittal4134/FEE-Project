7134114d28f1600a50053f169d5cfd3d61484366
sparshmittal4134 <45553780+sparshmittal4134@users.noreply.github.com>
2024-02-06 22:54:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function scrollToSection(sectionId) {
+    var section = document.getElementById(sectionId);
+    if (section) {
+        section.scrollIntoView({ behavior: "smooth" });
+    }
+}
+
+
